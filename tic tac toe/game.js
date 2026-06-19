8daed89849fbfeda4a1cd16a0726456b576833f4
Gagandeep <iam.gaganndeep.singh@gmail.com>
2026-06-19 08:14:04 +0530

    some changes has beed done

--- tic tac toe/game.js
+++ tic tac toe/game.js
@@ -1,7 +1,11 @@
 
 
-let boxes = document.querySelectorAll(".box"); // selecting all buttons inside the tic tac toe
-const replay = document.getElementById("reset"); // selecting bottom reset button 
+
+let boxes = document.querySelectorAll(".box");
+const replay = document.getElementById("reset"); 
+
+
+
 
 
 
