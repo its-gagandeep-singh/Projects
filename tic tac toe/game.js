d755200fd8c2eab96faeb25fdc4e35feea1d1c66
Gagandeep <iam.gaganndeep.singh@gmail.com>
2026-06-19 11:38:10 +0530

    => css : included box-shadow, fieldset border radius added and name fieldset removed <= , => js : logic to add  X and O when user click on buttons

--- tic tac toe/game.js
+++ tic tac toe/game.js
@@ -1,8 +1,49 @@
+let boxes = document.querySelectorAll(".box")
+let playerValue = true; // X
+
+const pattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// click to reset the game
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+    window.location.reload()
+    
+    // boxes.forEach((boxes) => {
+    //     if(boxes.innerText != "") 
+    //     {
+    //         boxes.innerText = ""
+    //     }
+    // })
+
+})
+
+boxes.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        // if box is empty only then add
+        if(button.innerText === "") 
+        {
+            if(playerValue) {
+                button.textContent = "X"
+                playerValue = false;
+            } else {
+                button.textContent = "O"
+                playerValue = true;
+            }
+        }
+    })
+})
 
 
 
-let boxes = document.querySelectorAll(".box");
-const replay = document.getElementById("reset"); 
 
 
 
@@ -15,3 +56,25 @@ const replay = document.getElementById("reset");
 
 
 
+    // toggle the background color of the second box (index 1)
+    // const box = document.querySelectorAll(".box")[1];
+    // if (!box) return;
+
+    // if (c === 1) {
+    //     box.style.backgroundColor = "red";
+    //     c = 2;
+    // } else {
+    //     box.style.backgroundColor = "royalblue";
+    //     c = 1;
+    // }
+
+
+    // querySelectorAll -> returns array called node list with values of html element shares same class //
+/*
+    // Event object (passed in event function parameter)
+
+1. event object has differenct type of method like target, type, key
+2. e.target -> tells which button is clicked
+*/
+
+// boxes = array = nodelist return by querySelectorAll // 
